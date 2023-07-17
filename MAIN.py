a44b373293798e2d13a9238a8d749588b7acfad2
Seba <134440670+sebastian-lopez-p@users.noreply.github.com>
2023-07-16 22:39:55 -0400

    Update MAIN.py
    Añadida la funcion interactiva de actualización del gráfico en base a un Slider de la variable m: (m2/m1).

--- MAIN.py
+++ MAIN.py
@@ -1,11 +1,12 @@
 
-#Snapshot 1.0.1
+#Snapshot 1.0.2
 
 #Importacion de funciones:
 
 import numpy as np
 import matplotlib.pylab as plt
 from PyAstronomy import pyasl
+from matplotlib.widgets import Slider
 
 #Funcion
 def puntos_lagrange(masa1,masa2,distancia):
@@ -15,7 +16,47 @@ def puntos_lagrange(masa1,masa2,distancia):
   de roche, y no en su obtención como tal.
   '''
   #De momento la distancia no tiene utilizad, pero se utilizará en un futuro
+  def update(val):
+    '''
+    Actualiza los parámetros del grafico en funcion del cambio en el Slider interactivo de la variable m: (m2/m1)
+    '''
+    global m, l1, l2, l3, l4, l5
+
+    m = slider_m.val
+    if m == 1:
+      # Establece un valor pequeño cercano a 1 para evitar errores en la función rochepot_dl
+      m = 0.99999999
+    
+    # Obtener valores dimensionales del potencial de Roche
+    p = pyasl.rochepot_dl(X, Y, 0, m) #potencial
   
+    #Puntos l1/l2/l3 y sus potenciales:
+    l1, l1pot = pyasl.get_lagrange_1(m)
+    l2, l2pot = pyasl.get_lagrange_2(m)
+    l3, l3pot = pyasl.get_lagrange_3(m)
+    #Puntos l4/l5 y sus potenciales
+    l4, l5 = pyasl.get_lagrange_4(), pyasl.get_lagrange_5()
+    l4pot = pyasl.rochepot_dl(l4[0], l4[1], l4[2], m)
+    l5pot = pyasl.rochepot_dl(l5[0], l5[1], l5[2], m)
+  
+    #grafico
+    level= l4pot*1.005, l3pot, l2pot, l1pot
+    levels = sorted(level)
+
+    ax.clear()
+    ax.contourf(p, levels, extent=[-2, 3, -2, 2])
+    ax.scatter(0,0,s=300, color='yellow',label='Masa 1', edgecolor='black')
+    ax.scatter(1,0,s=130,color='cyan', label='Masa 2', edgecolor='black')
+    ax.scatter(l1, 0, label='L1', edgecolor='black')
+    ax.scatter(l2, 0, label='L2', edgecolor='black')
+    ax.scatter(l3, 0, label='L3', edgecolor='black')
+    ax.scatter(l4[0], l4[1], label='L4', edgecolor='black')
+    ax.scatter(l5[0], l5[1], label='L5', edgecolor='black')
+    ax.legend()
+    ax.grid()
+    ax.set_title('Roche lobe  \n  Interactive menu')
+    
+    
   #Parametros:
   m1= masa1 #Objeto más pesado [Kg](central)
   m2= masa2 #Objeto más liviano [Kg]](el que orbite alrededor del objeto más pesado)
@@ -23,40 +64,31 @@ def puntos_lagrange(masa1,masa2,distancia):
 
   #Variables:
   m= m2/m1# m2/m1 #Mass ratio
+  M = m
   x, y = np.linspace(-2, 3, 300), np.linspace(-2, 2, 300) #Posición
   X, Y = np.meshgrid(x, y)
-  p = pyasl.rochepot_dl(X, Y, 0, m) #potencial
 
-  #Puntos l1/l2/l3 y sus potenciales:
-  l1, l1pot = pyasl.get_lagrange_1(m)
-  l2, l2pot = pyasl.get_lagrange_2(m)
-  l3, l3pot = pyasl.get_lagrange_3(m)
-  #Puntos l4/l5 y sus potenciales
-  l4, l5 = pyasl.get_lagrange_4(), pyasl.get_lagrange_5()
-  l4pot = pyasl.rochepot_dl(l4[0], l4[1], l4[2], m)
-  l5pot = pyasl.rochepot_dl(l5[0], l5[1], l5[2], m)
+  #Parametros del gráfico
+  fig, ax = plt.subplots()
+  plt.subplots_adjust(left=0.1, right=0.8, bottom=0.1)
 
-  #grafico
-  level= l4pot*1.005, l3pot, l2pot, l1pot
-  levels = sorted(level)
+  #Slider
+  slider_ax = plt.axes([0.85, 0.1, 0.155, 0.7775])  # [left, bottom, width, height]
+  slider_m = Slider(slider_ax, 'm2/m1', M, 1.0, valinit=m, valstep=0.0001, orientation="vertical")
 
-  plt.contourf(p, levels, extent=[-2, 3, -2, 2])
-  plt.scatter(0,0,s=300, color='yellow',label='Masa 1', edgecolor='black')
-  plt.scatter(1,0,s=130,color='cyan', label='Masa 2', edgecolor='black')
-  plt.scatter(l1, 0, label='L1', edgecolor='black')
-  plt.scatter(l2, 0, label='L2', edgecolor='black')
-  plt.scatter(l3, 0, label='L3', edgecolor='black')
-  plt.scatter(l4[0], l4[1], label='L4', edgecolor='black')
-  plt.scatter(l5[0], l5[1], label='L5', edgecolor='black')
-  plt.legend()
-  plt.grid()
-  plt.title('Roche lobe')
+  # Actualizar el gráfico cuando se cambia el valor del slider
+  slider_m.on_changed(update)
+
+  # Graficar los datos iniciales
+  update(m)
+
+  # Mostrar el gráfico
   plt.show()
-  print(m)
-  
+
   #Output:
   return {l1,l2,l3,l4,l5}
 
+#MAIN
 #Datos de ejemplo: (en un futuro debería ser el usuario el que ingrese estos valores)
 #Establecer en el input que m1 => m2
 moon = 7.3477e22 #[Kg]1.898e27#
